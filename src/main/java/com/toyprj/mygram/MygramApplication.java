b5515329149d22bfc042f2752a9542798fc40aa7
JSP <pjs12041204@gmail.com>
2022-05-01 17:29:57 +0900

    git init

---
+++ src/main/java/com/toyprj/mygram/MygramApplication.java
@@ -0,0 +1,13 @@
+package com.toyprj.mygram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygramApplication.class, args);
+	}
+
+}
